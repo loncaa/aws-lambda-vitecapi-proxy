ec19d70fe34adffa11ef370d31d22f86a3002b47
Antonio <antonio.loncar@kickstage.com>
2021-05-28 16:41:27 +0200

    implemented vitec service

---
+++ index.js
@@ -0,0 +1,35 @@
+const VitecService = require('./src/vitec/vitec_service');
+
+//zip -r function.zip .  
+
+const headers = {
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
+    "Accept": "*/*",
+    "Content-Type": 'application/json'
+}
+
+exports.handler = async (payload, _, __) => {
+
+    if (!payload) {
+        return {
+            statusCode: 300,
+            headers: headers,
+            body: {
+                message: 'Data not found.'
+            }
+        }
+    }
+
+    const response = await VitecService.createContact({ ...payload });
+    const { data } = response;
+
+    await VitecService.createSearchProfile(customerId, data, payload).catch(error => console.log('error', error));
+    
+    return {
+        statusCode: 200,
+        headers: headers,
+        body: data
+    }
+}
